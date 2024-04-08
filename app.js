f81f5242369bcb2d3c40159b52d9ebbfce413b22
vjesus <vinicius.jesus@businessintegrator.com.br>
2024-04-08 10:50:32 -0300

    Adicao da biblioteca do sql

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const express = require('express')
 const app = express()
 const PORT = process.env.PORT || 3000;
 
+const db server = 'Vini_PC'
+
+
 app.use(express.json)
 
 app.get('/', (req, res) => {
