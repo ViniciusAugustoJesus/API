eeeebaeccf7ead0d9c6437a0679048e133dadd60
vjesus <vinicius.jesus@businessintegrator.com.br>
2024-04-08 10:46:47 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const app = express()
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json)
+
+app.get('/', (req, res) => {
+    res.send("Bem-Vindo á minha primeira API")
+})
+
+app.get('/livros', (req, res) => {
+    const livros = ["Livro 1", "Livro 2"]
+    res.json(livros)
+})
+
+app.post('/livros', (req, res) => {
+    const newBook = req.body
+
+    res.status(201).send("Item adicionado com sucesso!")
+})
+
+app.listen(PORT, () => {
+    console.log(`Servidor rodando em http://localhost:${PORT}`);
+})
